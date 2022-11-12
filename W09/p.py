c5391d9e1b0d94ee0788705224a5c9c73b2de015
Elroe Woldemichael <elroebro@gmail.com>
2022-11-11 21:48:42 -0700

    commit 2

--- W09/p.py
+++ W09/p.py
@@ -1,8 +1,9 @@
-friends = []
-enter = ''
-
-while enter != 'end':
-    enter = input('Please enter a value: ')
-    friends.append(enter)
-
-print(friends)
+import csv
+students = {
+    "10-450-1203": "Jorge Soares",
+    "75-421-2310": "Abdul Ali",
+    "07-103-5621": "Michelle Davis"
+}
+print(students)
+students["81-298-9238"] = "Sama Patel"
+print(len(students))
